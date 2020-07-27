c56cfcbf025984cea397d4dbda9f63dadc3ef448
hardiyan.purnama.hp@gmail.com <smart_m07>
2020-07-27 22:33:31 +0700

    Membuat Fungsi

---
+++ segitiga.py
@@ -0,0 +1,8 @@
+"""
+Membuat Fungsi dengan contoh ksus menghitung luas segitiga
+luas_segitiga = alas * tinggi /2
+"""
+alas = 10
+tinggi = 6
+luas_segitiga = alas * tinggi /2
+print(f'Segitiga dengan alas {alas} dan tinggi {tinggi} memiliki luas {luas_segitiga}')
