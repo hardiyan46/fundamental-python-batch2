04dfa9b569d61dea64eee627e77b1d9a44612ab9
Hardiyan Purnama <hardiyan.purnama.hp@gmail.com>
2020-07-29 00:08:56 +0700

    penambahan use request

--- use_requests.py
+++ use_requests.py
@@ -4,6 +4,7 @@ url = 'https://detik.com'
 try:
     response =  requests.get(url)
     print(f'Berhasil {response}')
+    print(f'ISI {response.text}')
 except Exception as e:
     print('Ada Error Bro', e)
 print('program Berakhir')
