23af291403f5c5605def9247ece78a2c00b4d802
hardiyan.purnama.hp@gmail.com <smart_m07>
2020-07-28 23:44:40 +0700

    penambahan persegi panjang

--- geometri/persegi_panjang.py
+++ geometri/persegi_panjang.py
@@ -2,3 +2,6 @@ def hitung_luas_persegi_panjang(panjang, lebar):
     luas_persegi_panjang = panjang * lebar
     return luas_persegi_panjang
 
+def info():
+    return 'Modul Menghitung Luas Persegi Panjang'
+
