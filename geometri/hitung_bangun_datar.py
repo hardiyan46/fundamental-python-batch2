b8956e5e25c7cfd450f51f058c261ff217c8c534
hardiyan.purnama.hp@gmail.com <smart_m07>
2020-07-28 14:33:48 +0700

    penambahan persegi panjang

---
+++ geometri/hitung_bangun_datar.py
@@ -0,0 +1,7 @@
+from persegi_panjang import hitung_luas_persegi_panjang   #import fungsi yang ada pada file persegi panjang
+#from segitiga import hitung_luas_segitiga  #import fungsi dari file segitiga
+
+panjang = 20
+lebar = 30
+tinggi = 20
+print(hitung_luas_persegi_panjang(40,10,20))
--- segitiga.py
+++ segitiga.py
@@ -6,3 +6,26 @@ alas = 10
 tinggi = 6
 luas_segitiga = alas * tinggi /2
 print(f'Segitiga dengan alas {alas} dan tinggi {tinggi} memiliki luas {luas_segitiga}')
+
+# Contoh segitiga dengan copy paste
+alas = 32
+tinggi = 40
+luas_segitiga = alas * tinggi /2
+print(f'Segitiga dengan alas {alas} dan tinggi {tinggi} memiliki luas {luas_segitiga}')
+
+# Contoh hitung luas segitiga dengan FUNGSI
+def hitung_luas_segitiga(alas, tinggi):
+    luas_segitiga = alas * tinggi /2
+    return luas_segitiga
+print(f'Menghitung Luas segitiga dengan Fungsi adalah {hitung_luas_segitiga(10, 6)}')
+print(f'Menghitung Luas segitiga dengan Fungsi adalah {hitung_luas_segitiga(31, 20)}')
+
+#Contoh fungsi dengan variable alas dan tinggi
+def hitung_luas_segitiga(alas, tinggi):
+    luas_segitiga = alas * tinggi /2
+    return luas_segitiga
+alas = 10
+tinggi = 35
+
+print(f'Menghitung Luas segitiga dengan Fungsi adalah {hitung_luas_segitiga(alas, tinggi)}') #Mengambil dari nilai variabel
+print(f'Menghitung Luas segitiga dengan Fungsi adalah {hitung_luas_segitiga(31, 20)}') #Nilai variabel dimasukan pada argumen
